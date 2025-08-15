38cb8aef8b75933c99e6da4ab4d3ec74eece3e08
Mazzi <thiago.mazzi2017@gmail.com>
2025-08-15 09:38:03 -0300

    corrigindo bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import React, { createContext } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import Filmes from "./page/Filmes";
 import Series from "./page/Series";
@@ -9,7 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Login from "./components/Login.jsx";
 import LoginPage from "./page/LoginPage.jsx";
 import Resultado from "./page/Resultado.jsx";
 
